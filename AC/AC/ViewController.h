f4e73e7897bf4547a6407c2e392ce17297beb7f3
赵英奎 <zhaoyingkui@1.local>
2016-02-24 19:05:05 +0800

    1

---
+++ AC/AC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AC
+//
+//  Created by 赵英奎 on 16/2/24.
+//  Copyright (c) 2016年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AC/AC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AC
+//
+//  Created by 赵英奎 on 16/2/24.
+//  Copyright (c) 2016年 KK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
